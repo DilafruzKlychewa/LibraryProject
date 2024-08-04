c00f20adf269adf8bffbc52ea83f683b75ebb900
dilafruz <dilafruzk@gmail.com>
2024-08-04 07:16:31 +0300

    git commit "abc"
    git pull

--- src/main/java/com/tpe/LibraryProjectApplication.java
+++ src/main/java/com/tpe/LibraryProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibrayProjectApplication {
+public class LibraryProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibrayProjectApplication.class, args);
+		SpringApplication.run(LibraryProjectApplication.class, args);
 	}
 
 }
